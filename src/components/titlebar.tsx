e6244ab30667c8a5b417772eaf5198677b5e7164
ruwpy <ruwpy.who@gmail.com>
2023-07-07 16:39:11 +0200

    update styles

--- src/components/titlebar.tsx
+++ src/components/titlebar.tsx
@@ -5,21 +5,22 @@ import { appWindow } from "@tauri-apps/api/window";
 export const Titlebar = () => {
   return createPortal(
     <>
-      <span className="pointer-events-none ml-[10px] leading-0 text-neutral-600 text-[14px] font-semibold">
-        null-auth
+      <span className="flex items-center gap-[10px] pointer-events-none ml-[10px] text-neutral-900 text-[14px] font-medium">
+        <Icons.nullauth width={15} height={15} />
+        <span>null-auth</span>
       </span>
       <div className="flex">
         <div
           onClick={() => appWindow.minimize()}
-          className="hover:bg-neutral-800 active:bg-neutral-700 h-[30px] w-[30px] flex justify-center items-center"
+          className="hover:bg-black/10 active:bg-black/20 h-[30px] w-[30px] flex justify-center items-center"
         >
-          <Icons.minimize className="text-neutral-500" width={20} height={20} />
+          <Icons.minimize className="text-neutral-900" width={20} height={20} />
         </div>
         <div
           onClick={() => appWindow.close()}
-          className="hover:bg-neutral-800 active:bg-neutral-700 h-[30px] w-[30px] flex justify-center items-center"
+          className="hover:bg-black/10 active:bg-black/20 h-[30px] w-[30px] flex justify-center items-center"
         >
-          <Icons.close className="text-neutral-500" width={20} height={20} />
+          <Icons.close className="text-neutral-900" width={20} height={20} />
         </div>
       </div>
     </>,
