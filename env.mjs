222de962fd59abb20ee7546454e40963b4f02686
ruwpy <ruwpy.who@gmail.com>
2023-07-08 23:44:08 +0200

    fixed env

--- env.mjs
+++ env.mjs
@@ -2,12 +2,13 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
+  clientPrefix: "VITE_",
+  client: {
     VITE_SUPABASE_URL: z.string().url(),
     VITE_SUPABASE_SECRET: z.string()
   },
   runtimeEnv: {
-    VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
-    VITE_SUPABASE_SECRET: process.env.VITE_SUPABASE_SECRET
+    VITE_SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
+    VITE_SUPABASE_SECRET: import.meta.env.VITE_SUPABASE_SECRET
   }
 })
