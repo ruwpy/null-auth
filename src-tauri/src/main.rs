47d474cf838daa138503687a479321f36a7d32db
ruwpy <ruwpy.who@gmail.com>
2023-07-10 00:10:24 +0200

    create functions for encrypting and decrypting strings

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn hello(name: &str) -> String {
-    format!("ну здарова, {name}! давно тебя небыло в уличных гонках!")
-}
+use tauri_plugin_store;
+
+mod encryption;
+mod totp;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![hello])
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+            totp::generate_totp,
+            encryption::hash_string,
+            encryption::encrypt_string,
+            encryption::decrypt_string
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
